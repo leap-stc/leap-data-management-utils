b356b7d0bf242b339122eb6e2576ccc7a6d9a870
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-16 14:51:55 -0400

    Add convinience method to add dataframes

--- leap_data_management_utils/cmip_catalog.py
+++ leap_data_management_utils/cmip_catalog.py
@@ -42,3 +42,26 @@ def bq_df_to_intake_esm(df: pd.DataFrame) -> pd.DataFrame:
         ]
     ]
     return df_out
+
+def _maybe_join(iterable):
+    assert len(iterable) == 2 
+    sub_experiment_id = iterable.iloc[0]
+    variant_label = iterable.iloc[1]
+    if sub_experiment_id != 'none':
+        return f"{sub_experiment_id}-{variant_label}"
+    else:
+        return variant_label
+
+def intake_esm_df_to_bq_df(df: pd.DataFrame) -> pd.DataFrame:
+    # now remove the ones already in the pangeo catalog
+    df = df.copy()
+    df['member_id'] = df[["sub_experiment_id", "variant_label"]].agg(_maybe_join, axis=1)
+    df['version'] = 'v'+df['version'].astype(str)
+    df['instance_id'] = df[['activity_id', 'institution_id', 'source_id', 'experiment_id',
+        'member_id', 'table_id', 'variable_id', 'grid_label', 'version']].astype(str).agg('.'.join, axis=1).tolist()
+    df['instance_id'] = 'CMIP6.'+df['instance_id']
+    df['store'] = df['zstore']
+    # add current time as bigquery timestamp
+    df['timestamp'] = pd.Timestamp.now(tz='UTC')
+    df = df[['instance_id','store', 'timestamp']]
+    return df
