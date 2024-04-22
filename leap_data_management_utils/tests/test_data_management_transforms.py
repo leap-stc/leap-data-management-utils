4bf7925abc4e1c29453ed40c4ef550f3ad3ea114
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-22 17:51:30 -0400

    Add get_catalog_store_urls + get_github_commit_url (#23)
    * Add get_catalog_store_urls + get_github_commit_url
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix pre-commit stuff
    
    * Bugfix in get_catalog_store_urls
    
    * Make inject_attrs optional
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- leap_data_management_utils/tests/test_data_management_transforms.py
+++ leap_data_management_utils/tests/test_data_management_transforms.py
@@ -1,4 +1,36 @@
+from ruamel.yaml import YAML
+
+from leap_data_management_utils.data_management_transforms import (
+    get_catalog_store_urls,
+    get_github_commit_url,
+)
+
+yaml = YAML(typ='safe')
+
+
 def test_smoke_test():
     assert True
     # This is a bit dumb, but it at least checks the the imports are working
     # again super hard to test code involving bigquery here.
+
+
+def test_get_github_commit_url():
+    url = get_github_commit_url()
+    assert url.startswith('https://github.com/leap-stc/leap-data-management-utils')
+
+
+def test_get_catalog_store_urls(tmp_path):
+    # Create a temporary text file
+    temp_file = tmp_path / 'some-name.yaml'
+    data = {
+        'stores': [{'id': 'a', 'url': 'a-url', 'some_other': 'stuff'}, {'id': 'b', 'url': 'b-url'}]
+    }
+    with open(temp_file, 'w') as f:
+        yaml.dump(data, f)
+
+    # Call the function to read the file
+    content = get_catalog_store_urls(temp_file)
+
+    # Assertions
+    assert content['a'] == 'a-url'
+    assert content['b'] == 'b-url'
